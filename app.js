ac2b80acd1b5744f12d5247c1acf44ad31400319
root <minnamadhuri@gmail.com>
2022-12-06 10:17:18 +0000

    commit

---
+++ app.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+const format = require("date-fns/format");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+let db = null;
+const dbPath = path.join(__dirname, "twitterClone.db");
+const app = express();
+app.use(express.json());
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, (request, response) => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DataBase Error:${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    console.log(jwtToken + "1");
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        console.log(jwtToken + "2");
+        response.status(401);
+        response.send("Invalid JWT Token");
+      } else {
+        console.log(jwtToken + "3");
+        console.log(payload.username);
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
+
+app.post("/register/", async (request, response) => {
+  const { username, name, password, gender } = request.body;
+  const hashedPassword = await bcrypt.hash(request.body.password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    const createUserQuery = `
+      INSERT INTO 
+        user (username, name, password, gender) 
+      VALUES 
+        (
+          '${username}', 
+          '${name}',
+          '${hashedPassword}', 
+          '${gender}'
+        )`;
+    const dbResponse = await db.run(createUserQuery);
+    const newUserId = dbResponse.lastID;
+    response.send(`User created successfully`);
+  } else if (password.length < 6) {
+    response.status = 400;
+    response.send("Password is too short");
+  } else {
+    response.status = 400;
+    response.send("User already exists");
+  }
+});
+
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+app.get("/user/tweets/feed/", authenticateToken, async (request, response) => {
+  let { username } = request;
+
+  const result = `select username,tweet.tweet,tweet.date_time as dateTime 
+FROM user INNER JOIN tweet on user.user_id=tweet.user_id 
+where username='${username}'
+ORDER BY tweet.date_time DESC
+LIMIT 4;`;
+  const result1 = await db.all(result);
+  console.log(result1);
+  response.send(result1);
+});
+module.exports = app;
